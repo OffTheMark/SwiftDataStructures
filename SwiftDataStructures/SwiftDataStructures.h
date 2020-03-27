25b87761f26506e852eabe22f57147203fbffd6b
Marc-Antoine Malépart <marc-antoine.malepart@desjardins.com>
2020-03-27 09:21:46 -0400

    Initial Commit

---
+++ SwiftDataStructures/SwiftDataStructures.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDataStructures.h
+//  SwiftDataStructures
+//
+//  Created by Marc-Antoine Malépart on 2020-03-27.
+//  Copyright © 2020 OffTheMark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDataStructures.
+FOUNDATION_EXPORT double SwiftDataStructuresVersionNumber;
+
+//! Project version string for SwiftDataStructures.
+FOUNDATION_EXPORT const unsigned char SwiftDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDataStructures/PublicHeader.h>
+
+
