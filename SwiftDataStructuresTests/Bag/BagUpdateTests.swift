23fe82ffe38feca09f0e7f45410cd04a5b51defb
Marc-Antoine Malépart <marc-antoine.malepart@desjardins.com>
2020-04-09 15:35:55 -0400

    Add shorter tests that assert more specific side-effects

--- SwiftDataStructuresTests/Bag/BagUpdateTests.swift
+++ SwiftDataStructuresTests/Bag/BagUpdateTests.swift
@@ -60,13 +60,38 @@ final class BagUpdateTests: XCTestCase {
     
     // MARK: Removing Elements in an Empty Bag
     
-    func test_EmptyBase_AfterRemovingOneOfitem_IsEmpty() {
+    func test_EmptyBag_AfterRemovingOneOfitem_IsEmpty() {
         var bag = Bag<String>()
 
+        bag.remove("item")
+
+        XCTAssertTrue(bag.isEmpty)
+        XCTAssertEqual(bag, [:])
+    }
+    
+    func test_EmptyBag_AfterRemovingOneOfItem_ReturnsNilRemovedElements() {
+        var bag = Bag<String>()
+        
         let removed = bag.remove("item")
+        
+        XCTAssertNil(removed)
+    }
+    
+    func test_EmptyBag_AfterRemovingAllOfItem_IsEmpty() {
+        var bag = Bag<String>()
+
+        bag.removeAll(of: "item")
+
+        XCTAssertTrue(bag.isEmpty)
+        XCTAssertEqual(bag, [:])
+    }
+    
+    func test_EmptyBag_AfterRemovingAlOfItem_ReturnsNilRemovedElements() {
+        var bag = Bag<String>()
+
+        let removed = bag.removeAll(of: "item")
 
         XCTAssertNil(removed)
-        XCTAssertEqual(bag, [:])
     }
     
     // MARK: Removing Elements in a Non-Empty Bag
@@ -74,40 +99,94 @@ final class BagUpdateTests: XCTestCase {
     func test_BagWithOneOfAnItem_AfterRemovingOneOfItem_IsEmpty() {
         var bag: Bag = ["item"]
 
+        bag.remove("item")
+
+        XCTAssertTrue(bag.isEmpty)
+        XCTAssertEqual(bag, [:])
+    }
+    
+    func test_BagWithOneOfAnItem_AfterRemovingOneOfItem_ReturnsCorrectRemovedElements() {
+        var bag: Bag = ["item"]
+
         let removed = bag.remove("item")
 
         XCTAssertEqual(removed?.item, "item")
         XCTAssertEqual(removed?.count, 1)
-        XCTAssertEqual(bag, [:])
     }
 
     func test_BagWithMultiplesOfAnItem_AfterRemovingLessThanCountOfItem_ContainsCorrectNumberOfItems() {
+        var bag: Bag = ["item": 5]
+
+        bag.remove("item", count: 2)
+
+        XCTAssertEqual(bag.count, 1)
+        XCTAssertEqual(bag, ["item": 3])
+    }
+
+    func test_BagWithMultiplesOfAnItem_AfterRemovingLessThanCountOfItem_ReturnsCorrectRemovedElements() {
         var bag: Bag = ["item": 5]
 
         let removed = bag.remove("item", count: 2)
 
         XCTAssertEqual(removed?.item, "item")
         XCTAssertEqual(removed?.count, 2)
-        XCTAssertEqual(bag, ["item": 3])
     }
 
     func test_BagWithMultiplesOfAnItem_AfterRemovingSameNumberOfItem_IsEmpty() {
+        var bag: Bag = ["item": 2]
+
+        bag.remove("item", count: 2)
+        
+        XCTAssertTrue(bag.isEmpty)
+        XCTAssertEqual(bag, [:])
+    }
+    
+    func test_BagWithMultiplesOfAnItem_AfterRemovingSameNumberOfItem_ReturnsCorrectRemovedElement() {
         var bag: Bag = ["item": 2]
 
         let removed = bag.remove("item", count: 2)
         
         XCTAssertEqual(removed?.item, "item")
         XCTAssertEqual(removed?.count, 2)
+    }
+    
+    func test_BagWithMultiplesOfAnItem_AfterRemovingMoreItemsThanAreContained_IsEmpty() {
+        var bag: Bag = ["item": 2]
+
+        bag.remove("item", count: 10)
+        
+        XCTAssertTrue(bag.isEmpty)
         XCTAssertEqual(bag, [:])
+    }
+    
+    func test_BagWithMultiplesOfAnItem_AfterRemovingMoreItemsThanAreContained_ReturnsCorrectRemovedElement() {
+        var bag: Bag = ["item": 2]
+
+        let removed = bag.remove("item", count: 10)
+        
+        XCTAssertEqual(removed?.item, "item")
+        XCTAssertEqual(
+            removed?.count,
+            2,
+            "The count of removed elements is the number of items that are actually removed from the bag, which is not necessarily the given number of items to remove."
+        )
     }
 
     func test_BagWithMultiplesOfAnItem_AfterRemovingAllOfItem_IsEmpty() {
+        var bag: Bag = ["item": 2]
+
+        bag.removeAll(of: "item")
+        
+        XCTAssertTrue(bag.isEmpty)
+        XCTAssertEqual(bag, [:])
+    }
+
+    func test_BagWithMultiplesOfAnItem_AfterRemovingAllOfItem_ReturnsCorrectRemovedElement() {
         var bag: Bag = ["item": 2]
 
         let removed = bag.removeAll(of: "item")
         
         XCTAssertEqual(removed?.item, "item")
         XCTAssertEqual(removed?.count, 2)
-        XCTAssertEqual(bag, [:])
     }
 }
