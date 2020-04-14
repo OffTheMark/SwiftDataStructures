4c16a0aba66998aa9d4ee39f58e004fcd7b2a7a3
Marc-Antoine Malépart <marc-antoine.malepart@desjardins.com>
2020-04-14 10:57:37 -0400

    Add SortCriterion and extensions to sort by criteria

---
+++ SwiftDataStructures/Sources/SortExtensions.swift
@@ -0,0 +1,85 @@
+//
+//  SortExtensions.swift
+//  SwiftDataStructures
+//
+//  Created by Marc-Antoine Malépart on 2020-04-14.
+//  Copyright © 2020 OffTheMark. All rights reserved.
+//
+
+import Foundation
+
+// MARK: SortOrder
+
+public enum SortOrder {
+    case ascending
+    case descending
+    
+    public func makeComparator<T: Comparable>() -> (T, T) -> Bool {
+        switch self {
+        case .ascending:
+            return (<)
+            
+        case .descending:
+            return (>)
+        }
+    }
+}
+
+// MARK: - SortCriterion
+
+public struct SortCriterion<Element> {
+    public let areInIncreasingOrder: (Element, Element) -> Bool
+    
+    public let areInEqualOrder: (Element, Element) -> Bool
+    
+    public init(
+        areInIncreasingOrder: @escaping (Element, Element) -> Bool,
+        areInEqualOrder: @escaping (Element, Element) -> Bool
+    ) {
+        self.areInIncreasingOrder = areInIncreasingOrder
+        self.areInEqualOrder = areInEqualOrder
+    }
+    
+    public init<Value: Comparable>(
+        keyPath: KeyPath<Element, Value>,
+        order: SortOrder = .ascending
+    ) {
+        let valueComparator: (Value, Value) -> Bool = order.makeComparator()
+        let areInIncreasingOrder: (Element, Element) -> Bool = { first, second in
+            return valueComparator(first[keyPath: keyPath], second[keyPath: keyPath])
+        }
+        let areInEqualOrder: (Element, Element) -> Bool = { first, second in
+            return first[keyPath: keyPath] == second[keyPath: keyPath]
+        }
+        
+        self.init(areInIncreasingOrder: areInIncreasingOrder, areInEqualOrder: areInEqualOrder)
+    }
+}
+
+// MARK: - Sequence
+
+extension Sequence {
+    func sorted<Value: Comparable>(
+        by keyPath: KeyPath<Element, Value>,
+        order: SortOrder = .ascending
+    ) -> [Element] {
+        let criterion = SortCriterion(keyPath: keyPath, order: order)
+        return self.sorted(by: [criterion])
+    }
+    
+    func sorted(by criteria: [SortCriterion<Element>]) -> [Element] {
+        let areInIncreasingOrder: (Element, Element) -> Bool = { first, second in
+            for criterion in criteria {
+                if criterion.areInEqualOrder(first, second) {
+                    continue
+                }
+                
+                return criterion.areInIncreasingOrder(first, second)
+            }
+            
+            return false
+        }
+        
+        return self.sorted(by: areInIncreasingOrder)
+    }
+}
